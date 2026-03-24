cb65c96b6a5fff4ecbdf4b64077f87d6ed9b0543
dewald23 <dewaldwiid@gmail.com>
2026-03-24 10:02:46 +0200

    feat: Remove redundant ContactSupport component
    The ContactSupport component was a duplicate of the existing Contact page and was only rendered in the App.tsx layout. This commit removes the component to avoid redundancy and simplify the application structure.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import { useEffect } from 'react';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import { ContactSupport } from './components/ContactSupport';
 import { Home } from './pages/Home';
 import { ServicesPage } from './pages/ServicesPage';
 import { About } from './pages/About';
@@ -34,7 +33,6 @@ export default function App() {
             <Route path="/blog/:slug" element={<BlogPost />} />
           </Routes>
         </main>
-        <ContactSupport />
         <Footer />
       </div>
     </Router>
